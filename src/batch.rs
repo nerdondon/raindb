0b13b651f5c0bed1d4a5b5902a9136d763e13ac9
nerdondon <willdon@nerdondon.com>
2021-10-06 07:09:19 +0000

    Add docs and sequence number to batch object

--- src/batch.rs
+++ src/batch.rs
@@ -1,5 +1,14 @@
 /*!
 This module contains representations and utility functions for batch functions.
+
+The elements of a batch request are described primarily by the type of operation being performed,
+a user supplied key, and--optionally--a user suppplied value. The value is optional in the case of a
+delete operation. In industry parlance, the delete operation denotes a "tombstone". A sequence
+number is also associated with the operation. The sequence number is a global, monotonically
+increasing 64-bit unsigned int. It is never reset.
+
+For storage, a batch is converted to binary with `bincode` which has it's own enconding
+specification.
 */
 
 /** The operation that is being applied to the data of a batch entry. */
@@ -25,7 +34,16 @@ pub(crate) struct BatchElement {
     key: Key,
     ///The value to set on the key for `Put` operations or `None` for delete operations.
     value: Option<Value>,
+    /// A sequence number associated with the operation.
+    sequence_num: u64,
 }
 
 /** A set of operations to perform atomically. */
-pub(crate) type Batch = Vec<BatchElement>;
+pub(crate) struct Batch {
+    /// The starting sequence number for this batch of operations.
+    starting_seq_num: u64,
+    /// A list of operations to perform in a batch.
+    operations: Vec<BatchElement>,
+}
+
+impl Batch {}
