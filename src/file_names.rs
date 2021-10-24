a9e405fad143dce8ea5782ee0458989f9f201995
nerdondon <willdon@nerdondon.com>
2021-10-24 07:30:11 +0000

    Fix: Various issues including const initialization and more consistent naming of `buf`

--- src/file_names.rs
+++ src/file_names.rs
@@ -5,6 +5,7 @@ Files are rooted at the `db_path` as provided in the [database instantiation opt
 
 Files (and their name formats) used by the database are as follows:
 
+- Database lock file: `./LOCK`
 - Write-ahead logs: `./wal/wal.log`
 - SSTable files: `./data/.sst`
 - MANIFEST files: `./MANIFEST`
@@ -12,17 +13,20 @@ Files (and their name formats) used by the database are as follows:
 
 use std::path::PathBuf;
 
+/// The name of the database lock file.
+pub(crate) const LOCK_FILE: &str = "LOCK";
+
 /// The directory name that write-ahead logs will be stored in.
-pub(crate) const WAL_DIR: String = "wal".to_string();
+pub(crate) const WAL_DIR: &str = "wal";
 
 /// Suffix for write-ahead log files.
-pub(crate) const WAL_EXT: String = "log".to_string();
+pub(crate) const WAL_EXT: &str = "log";
 
 /// The directory name that data files will be stored in.
-pub(crate) const DATA_DIR: String = "data".to_string();
+pub(crate) const DATA_DIR: &str = "data";
 
 /// Suffix for SSTable files.
-pub(crate) const SST_EXT: String = "sst".to_string();
+pub(crate) const SST_EXT: &str = "sst";
 
 pub(crate) struct FileNameHandler {
     db_path: String,
@@ -34,11 +38,11 @@ impl FileNameHandler {
     }
 
     pub fn get_wal_path(&self) -> PathBuf {
-        let buff = PathBuf::from(&self.db_path);
-        buff.push(WAL_DIR);
-        buff.set_file_name("wal");
-        buff.set_extension(WAL_EXT);
+        let mut buf = PathBuf::from(&self.db_path);
+        buf.push(WAL_DIR);
+        buf.set_file_name("wal");
+        buf.set_extension(WAL_EXT);
 
-        buff
+        buf
     }
 }
