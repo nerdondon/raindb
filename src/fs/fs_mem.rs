c6622ecf46bad1f9864d6e23977f44630baa0acf
nerdondon <willdon@nerdondon.com>
2021-10-27 07:25:46 +0000

    Wrap the entire file struct in a lock instead of just locking the contents

--- src/fs/fs_mem.rs
+++ src/fs/fs_mem.rs
@@ -18,7 +18,7 @@ pub struct InMemoryFileSystem {
     Currently using the familiar Arc<RwLock<>> construct but maybe look into replacing with an
     actual concurrent hashmap. Low priority since in-mem is mainly for testing.
     */
-    pub files: Arc<RwLock<HashMap<PathBuf, InMemoryFile>>>,
+    files: Arc<RwLock<HashMap<PathBuf, LockableInMemoryFile>>>,
 }
 
 impl InMemoryFileSystem {
@@ -30,26 +30,31 @@ impl InMemoryFileSystem {
     }
 }
 
+impl Default for InMemoryFileSystem {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 /// Private methods.
 impl InMemoryFileSystem {
     /**
     Open the file at the specified `path`.
 
-    This method returns the internal representation of an [in-memory file](self::InMemoryFile) and
-    is meant for use as an utility since the trait implementations return super-types.
+    This method returns the internal representation of an [in-memory file](self::InMemoryFile)
+    wrapped with a lock and is meant for use as an utility since the trait implementations return
+    super-types.
     */
-    fn open_mem_file(&self, path: &Path) -> io::Result<InMemoryFile> {
+    fn open_mem_file(&self, path: &Path) -> io::Result<LockableInMemoryFile> {
         let files = self.files.read();
         match files.get(path) {
-            Some(file) => {
-                return Ok(file.clone());
-            }
+            Some(file) => Ok(file.clone()),
             None => {
                 let error_message = format!(
                     "Could not find the file with path {path}",
                     path = path.to_string_lossy()
                 );
-                return Err(io::Error::new(io::ErrorKind::NotFound, error_message));
+                Err(io::Error::new(io::ErrorKind::NotFound, error_message))
             }
         }
     }
@@ -57,7 +62,7 @@ impl InMemoryFileSystem {
 
 impl FileSystem for InMemoryFileSystem {
     fn get_name(&self) -> String {
-        return "InMemoryFileSystem".to_string();
+        "InMemoryFileSystem".to_string()
     }
 
     fn create_dir(&mut self, _path: &Path) -> io::Result<()> {
@@ -74,7 +79,7 @@ impl FileSystem for InMemoryFileSystem {
         let mut children: Vec<PathBuf> = files
             .keys()
             .filter(|key| key.starts_with(path))
-            .map(|key| key.clone())
+            .cloned()
             .collect();
         children.sort();
 
@@ -106,15 +111,12 @@ impl FileSystem for InMemoryFileSystem {
     fn create_file(&self, path: &Path) -> io::Result<Box<dyn RandomAccessFile>> {
         let mut files = self.files.write();
         match files.get_mut(path) {
-            Some(file) => {
-                return Ok(Box::new(file.clone()));
-            }
+            Some(file) => Ok(Box::new(file.clone())),
             None => {
-                let error_message = format!(
-                    "Could not find the file with path {path}",
-                    path = path.to_string_lossy()
-                );
-                return Err(io::Error::new(io::ErrorKind::NotFound, error_message));
+                let new_file = LockableInMemoryFile::new();
+                files.insert(path.to_path_buf(), new_file);
+                let file = files.get(path).unwrap();
+                Ok(Box::new(file.clone()))
             }
         }
     }
@@ -122,21 +124,19 @@ impl FileSystem for InMemoryFileSystem {
     fn remove_file(&self, path: &Path) -> io::Result<()> {
         let mut files = self.files.write();
         match files.remove(path) {
-            Some(_removed_file) => {
-                return Ok(());
-            }
+            Some(_removed_file) => Ok(()),
             None => {
                 let error_message = format!(
                     "Could not find the file with path {path}",
                     path = path.to_string_lossy()
                 );
-                return Err(io::Error::new(io::ErrorKind::NotFound, error_message));
+                Err(io::Error::new(io::ErrorKind::NotFound, error_message))
             }
         }
     }
 
     fn get_file_size(&self, path: &Path) -> io::Result<u64> {
-        Ok(self.open_mem_file(path)?.contents.read().len() as u64)
+        Ok(self.open_mem_file(path)?.len() as u64)
     }
 }
 
@@ -144,7 +144,7 @@ impl FileSystem for InMemoryFileSystem {
 #[derive(Clone)]
 struct InMemoryFile {
     /// The contents of the file.
-    contents: Arc<RwLock<Vec<u8>>>,
+    contents: Vec<u8>,
     /// The current position in the file.
     cursor: u64,
 }
@@ -153,45 +153,63 @@ impl InMemoryFile {
     /// Create an instance of [`InMemoryFile`](self::InMemoryFile)
     fn new() -> Self {
         Self {
-            contents: Arc::new(RwLock::new(vec![])),
+            contents: vec![],
             cursor: 0,
         }
     }
 
     /// Get the size of the file in bytes.
     pub fn len(&self) -> u64 {
-        let contents = self.contents.read();
-        contents.len() as u64
+        self.contents.len() as u64
     }
 }
 
-impl Read for InMemoryFile {
+struct LockableInMemoryFile(Arc<RwLock<InMemoryFile>>);
+
+impl LockableInMemoryFile {
+    /// Create an instance of [`LockableInMemoryFile`](self::LockableInMemoryFile).
+    fn new() -> Self {
+        LockableInMemoryFile(Arc::new(RwLock::new(InMemoryFile::new())))
+    }
+
+    /// Get the size of the file in bytes.
+    pub fn len(&self) -> u64 {
+        self.0.read().len() as u64
+    }
+
+    /// Make an `Arc` clone of the file.
+    fn clone(&self) -> LockableInMemoryFile {
+        LockableInMemoryFile(Arc::clone(&self.0))
+    }
+}
+
+impl Read for LockableInMemoryFile {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let buf_length = buf.len();
         if buf_length == 0 {
             return Ok(0);
         }
 
-        let file_contents = self.contents.read();
-        buf.copy_from_slice(&file_contents[..buf_length]);
+        let file = self.0.read();
+        buf.copy_from_slice(&file.contents[..buf_length]);
 
         Ok(buf_length)
     }
 }
 
-impl Write for InMemoryFile {
+impl Write for LockableInMemoryFile {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let buf_length = buf.len();
         if buf_length == 0 {
             return Ok(0);
         }
 
-        let mut file_contents = self.contents.write();
+        let mut file = self.0.write();
         // `copy_from_slice` only allows copying between slices of the same length
-        let file_content_length = file_contents.len();
-        file_contents.copy_from_slice(&buf[0..file_content_length]);
+        let content_length = file.contents.len();
+        file.contents.copy_from_slice(&buf[0..content_length]);
         // Append the rest of the buffer
-        file_contents.extend_from_slice(&buf[file_content_length..]);
+        file.contents.extend_from_slice(&buf[content_length..]);
 
         Ok(buf_length)
     }
@@ -201,7 +219,7 @@ impl Write for InMemoryFile {
     }
 }
 
-impl Seek for InMemoryFile {
+impl Seek for LockableInMemoryFile {
     /**
     Seek to an offset, in bytes, in a stream.
 
@@ -209,6 +227,8 @@ impl Seek for InMemoryFile {
     positive integers.
     */
     fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+        let mut file = self.0.write();
+
         let mut offset: u64;
         match pos {
             SeekFrom::Start(off) => {
@@ -223,7 +243,7 @@ impl Seek for InMemoryFile {
                     return Err(io::Error::new(io::ErrorKind::InvalidInput, error_message));
                 }
 
-                offset = (off as u64) + self.cursor;
+                offset = (off as u64) + file.cursor;
             }
             SeekFrom::End(_) => {
                 unimplemented!("Not used as part of any database operations.");
@@ -231,18 +251,18 @@ impl Seek for InMemoryFile {
         };
 
         // Truncate `offset` if it is too long. We only allow seeking to the end of the file.
-        offset = if offset > self.len() {
-            self.len()
+        offset = if offset > file.len() {
+            file.len()
         } else {
             offset
         };
 
-        self.cursor = offset;
+        file.cursor = offset;
         Ok(offset)
     }
 }
 
-impl ReadonlyRandomAccessFile for InMemoryFile {
+impl ReadonlyRandomAccessFile for LockableInMemoryFile {
     fn read_from(&self, buf: &mut [u8], offset: usize) -> io::Result<usize> {
         let buf_length = buf.len();
         if buf_length == 0 {
@@ -256,25 +276,25 @@ impl ReadonlyRandomAccessFile for InMemoryFile {
             ));
         }
 
-        let file_contents = self.contents.read();
-        let end_of_copy_range = if file_contents.len() > (offset + buf.len()) {
+        let file = self.0.read();
+        let end_of_copy_range = if file.contents.len() > (offset + buf.len()) {
             offset + buf.len()
         } else {
-            file_contents.len()
+            file.contents.len()
         };
 
         // `copy_from_slice` requires that slices are of the same length or it panics. this is the
         // reason for the range syntax below.
-        (&mut buf[..offset]).copy_from_slice(&file_contents[offset..end_of_copy_range]);
+        (&mut buf[..offset]).copy_from_slice(&file.contents[offset..end_of_copy_range]);
 
         Ok(buf_length)
     }
 }
 
-impl RandomAccessFile for InMemoryFile {
+impl RandomAccessFile for LockableInMemoryFile {
     fn append(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let mut content = self.contents.write();
-        content.extend_from_slice(&buf);
+        let mut file = self.0.write();
+        file.contents.extend_from_slice(buf);
         Ok(buf.len())
     }
 }
