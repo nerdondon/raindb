3fcf72b8e68245a2109f818bcbb26819d60ee100
nerdondon <willdon@nerdondon.com>
2021-11-08 06:00:49 +0000

    Add utility for generating table file names

--- src/file_names.rs
+++ src/file_names.rs
@@ -25,23 +25,39 @@ pub(crate) const WAL_EXT: &str = "log";
 /// The directory name that data files will be stored in.
 pub(crate) const DATA_DIR: &str = "data";
 
-/// Suffix for SSTable files.
-pub(crate) const SST_EXT: &str = "sst";
+/// Suffix for table files.
+pub(crate) const TABLE_EXT: &str = "rdb";
 
+/// The name of the manifest file.
+pub(crate) const MANIFEST_FILE: &str = "MANIFEST";
+
+/// Various utilities for managing file and folder names that RainDB uses.
 pub(crate) struct FileNameHandler {
     db_path: String,
 }
 
 impl FileNameHandler {
+    /// Create a new instance of the [`FileNameHandler`].
     pub fn new(db_path: String) -> Self {
         FileNameHandler { db_path }
     }
 
+    /// Resolve the path to the write-ahead log.
     pub fn get_wal_path(&self) -> PathBuf {
         let mut buf = PathBuf::from(&self.db_path);
         buf.push(WAL_DIR);
         buf.set_file_name("wal");
         buf.set_extension(WAL_EXT);
+
+        buf
+    }
+
+    /// Resolve the path to a specific table file.
+    pub fn get_table_file_name(&self, file_number: u64) -> PathBuf {
+        let mut buf = PathBuf::from(&self.db_path);
+        buf.push(DATA_DIR);
+        buf.set_file_name(file_number.to_string());
+        buf.set_extension(TABLE_EXT);
 
         buf
     }
