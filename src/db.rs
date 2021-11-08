f37e6646f66262cf2f8b2a4185bc92aa9e7f6fda
nerdondon <willdon@nerdondon.com>
2021-11-08 06:00:27 +0000

    Fix a bunch of reference issues and use Rc/RefCell to share the filesystem

--- src/db.rs
+++ src/db.rs
@@ -2,12 +2,14 @@
 The database module contains the primary API for interacting with the key-value store.
 */
 
-use std::io;
+use std::cell::RefCell;
 use std::path::PathBuf;
+use std::rc::Rc;
 
+use crate::errors::RainDBResult;
 use crate::file_names::{DATA_DIR, WAL_DIR};
 use crate::fs::{FileSystem, OsFileSystem};
-use crate::memtable::MemTable;
+use crate::memtable::{MemTable, SkipListMemTable};
 use crate::write_ahead_log::WALWriter;
 
 /**
@@ -53,7 +55,7 @@ pub struct DbOptions {
 
     **This defaults to [`OsFileSystem`](crate::fs::OsFileSystem)**
     */
-    filesystem_provider: Box<dyn FileSystem>,
+    filesystem_provider: Rc<RefCell<Box<dyn FileSystem>>>,
 }
 
 impl Default for DbOptions {
@@ -66,50 +68,52 @@ impl Default for DbOptions {
                 .to_owned(),
             write_buffer_size: 4 * 1024 * 1024,
             max_file_size: 2 * 1024 * 1024,
-            filesystem_provider: Box::new(OsFileSystem::new()),
+            filesystem_provider: Rc::new(RefCell::new(Box::new(OsFileSystem::new()))),
         }
     }
 }
 
-pub struct DB<'fs> {
+pub struct DB {
     options: DbOptions,
-    memtable: MemTable,
-    wal: WALWriter<'fs>,
+    memtable: Box<dyn MemTable>,
+    wal: WALWriter,
 }
 
-impl DB<'_> {
-    pub fn open<'fs>(options: DbOptions) -> Result<DB<'fs>, io::Error> {
+impl DB {
+    pub fn open(options: DbOptions) -> RainDBResult<DB> {
         log::info!(
             "Initializing raindb with the following options {:#?}",
             options
         );
 
-        let DbOptions {
-            filesystem_provider: fs,
-            db_path,
-            ..
-        } = options;
+        let fs = Rc::clone(&options.filesystem_provider);
+        let db_path = &options.db_path;
 
         // Create DB root directory
         log::info!("Creating DB root directory at {}.", &db_path);
         let root_path = PathBuf::from(&db_path);
-        let wal_path = PathBuf::from(&db_path);
+        let mut wal_path = PathBuf::from(&db_path);
         wal_path.push(WAL_DIR);
-        let data_path = PathBuf::from(&db_path);
+        let mut data_path = PathBuf::from(&db_path);
         data_path.push(DATA_DIR);
 
-        fs.create_dir_all(&root_path);
-        fs.create_dir(&wal_path);
-        fs.create_dir(&data_path);
+        fs.borrow_mut().create_dir_all(&root_path)?;
+        fs.borrow_mut().create_dir(&wal_path)?;
+        fs.borrow_mut().create_dir(&data_path)?;
 
         // Create WAL
-        let wal = WALWriter::new(&fs, wal_path.to_str().unwrap())?;
+        let wal = WALWriter::new(Rc::clone(&fs), wal_path.to_str().unwrap())?;
 
         // Create memtable
+        let memtable = Box::new(SkipListMemTable::new());
 
         // Start compaction service
 
-        Ok(DB { options, wal })
+        Ok(DB {
+            options,
+            wal,
+            memtable,
+        })
     }
 
     pub fn get(&self) {
