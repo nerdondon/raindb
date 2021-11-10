0e1e52b1708b30c722329aa873cc2f5473ad77a2
nerdondon <willdon@nerdondon.com>
2021-11-10 08:51:47 +0000

    Add a reader for write-ahead logs

--- src/write_ahead_log.rs
+++ src/write_ahead_log.rs
@@ -18,9 +18,11 @@ data in subsequent blocks.
 
 use bincode::Options;
 use serde::{Deserialize, Serialize};
+use std::cell::RefCell;
 use std::convert::TryFrom;
 use std::io::{self, ErrorKind, SeekFrom, Write};
 use std::path::Path;
+use std::rc::Rc;
 
 use crate::errors::WALIOError;
 use crate::file_names::FileNameHandler;
@@ -77,10 +79,20 @@ impl From<&BlockRecord> for Vec<u8> {
     }
 }
 
+impl TryFrom<&Vec<u8>> for BlockRecord {
+    type Error = bincode::Error;
+
+    fn try_from(value: &Vec<u8>) -> bincode::Result<BlockRecord> {
+        bincode::DefaultOptions::new()
+            .with_fixint_encoding()
+            .deserialize(value)
+    }
+}
+
 /** Handles all write activity to the write-ahead log. */
-pub(crate) struct WALWriter<'fs> {
+pub(crate) struct WALWriter {
     /** A wrapper around a particular file system to use. */
-    fs: &'fs Box<dyn FileSystem>,
+    fs: Rc<RefCell<Box<dyn FileSystem>>>,
 
     /** The underlying file representing the WAL.  */
     wal_file: Box<dyn RandomAccessFile>,
@@ -98,23 +110,26 @@ pub(crate) struct WALWriter<'fs> {
 }
 
 /// Public methods.
-impl<'fs> WALWriter<'fs> {
+impl WALWriter {
     /**
     Construct a new `WALWriter`.
 
     * `fs`- The wrapped file system to use for I/O.
     * `db_path` - The absolute path where the database files reside.
     */
-    pub fn new<P: AsRef<Path>>(fs: &'fs Box<dyn FileSystem>, db_path: P) -> WALIOResult<Self> {
+    pub fn new<P: AsRef<Path>>(
+        fs: Rc<RefCell<Box<dyn FileSystem>>>,
+        db_path: P,
+    ) -> WALIOResult<Self> {
         let file_name_handler =
             FileNameHandler::new(db_path.as_ref().to_str().unwrap().to_string());
         let wal_path = file_name_handler.get_wal_path();
 
         log::info!("Creating WAL file at {}", wal_path.as_path().display());
-        let wal_file = fs.create_file(wal_path.as_path())?;
+        let wal_file = fs.borrow_mut().create_file(wal_path.as_path())?;
 
         let mut block_offset = 0;
-        let wal_file_size = fs.get_file_size(&wal_path)? as usize;
+        let wal_file_size = fs.borrow_mut().get_file_size(&wal_path)? as usize;
         if wal_file_size > 0 {
             block_offset = wal_file_size % BLOCK_SIZE_BYTES;
         }
@@ -175,7 +190,7 @@ impl<'fs> WALWriter<'fs> {
 }
 
 /// Private methods.
-impl<'fs> WALWriter<'fs> {
+impl WALWriter {
     /// Write the block out to the underlying medium.
     fn emit_block(&mut self, block_type: BlockType, data_chunk: &[u8]) -> WALIOResult<()> {
         // Convert `usize` to `u16` so that it fits in our header format.
@@ -199,5 +214,176 @@ impl<'fs> WALWriter<'fs> {
         self.current_cursor_position += bytes_written;
         log::info!("Wrote {} bytes to the WAL.", bytes_written);
         Ok(())
+    }
+}
+
+/** Handles all read activity to the write-ahead log. */
+pub(crate) struct WALReader {
+    /** A wrapper around a particular file system to use. */
+    fs: Rc<RefCell<Box<dyn FileSystem>>>,
+
+    /** The underlying file representing the WAL.  */
+    wal_file: Box<dyn ReadonlyRandomAccessFile>,
+
+    /** Handler for databases filenames. */
+    file_name_handler: FileNameHandler,
+
+    /**
+    An initial offset to start reading the WAL from.
+
+    This can be a byte offset and is not necessarily aligned the start of a block.
+    */
+    initial_offset: usize,
+
+    /**
+    The offset to the byte being read.
+
+    This should usually be aligned to the start of a block.
+    */
+    current_cursor_position: usize,
+}
+
+/// Public methods.
+impl WALReader {
+    /**
+    Construct a new `WALReader`.
+
+    * `fs`- The wrapped file system to use for I/O.
+    * `db_path` - The absolute path where the database files reside.
+    * `initial_block_offset` - An initial offset to start reading the WAL from.
+    */
+    pub fn new<P: AsRef<Path>>(
+        fs: Rc<RefCell<Box<dyn FileSystem>>>,
+        db_path: P,
+        initial_block_offset: usize,
+    ) -> WALIOResult<Self> {
+        let file_name_handler =
+            FileNameHandler::new(db_path.as_ref().to_str().unwrap().to_string());
+        let wal_path = file_name_handler.get_wal_path();
+
+        log::info!("Reading the WAL file at {}", wal_path.as_path().display());
+        let wal_file = fs.borrow().open_file(wal_path.as_path())?;
+
+        let reader = Self {
+            fs,
+            wal_file,
+            file_name_handler,
+            initial_offset: initial_block_offset,
+            current_cursor_position: initial_block_offset,
+        };
+
+        Ok(reader)
+    }
+
+    /// Read a record from the WAL.
+    pub fn read_record(&mut self) -> WALIOResult<Vec<u8>> {
+        self.current_cursor_position += self.seek_to_initial_block()? as usize;
+        // A buffer consolidating all of the fragments retrieved from the WAL file.
+        let mut data_buffer: Vec<u8> = vec![];
+
+        loop {
+            let record = self.read_physical_record()?;
+            data_buffer.extend(record.data);
+
+            match record.block_type {
+                BlockType::Full => {
+                    return Ok(data_buffer);
+                }
+                BlockType::First => {}
+                BlockType::Middle => {}
+                BlockType::Last => {
+                    return Ok(data_buffer);
+                }
+            }
+        }
+    }
+}
+
+/// Private methods.
+impl WALReader {
+    /**
+    Seek to the first block on or before the initial offset that was provided.
+
+    The initial offset may land in the middle of a block's byte range. We will look backwards to
+    find the start of the block if this happens. Corrupted data will be logged and skipped.
+
+    Returns the number of bytes the cursor was moved.
+    */
+    fn seek_to_initial_block(&mut self) -> WALIOResult<u64> {
+        let offset_in_block = self.initial_offset % BLOCK_SIZE_BYTES;
+        let mut block_start_position = self.initial_offset - offset_in_block;
+
+        // Skip ahead if we landed in the trailer.
+        if offset_in_block > BLOCK_SIZE_BYTES - 2 {
+            block_start_position += BLOCK_SIZE_BYTES;
+        }
+
+        // Move the file's underlying cursor to the start of the first block
+        if block_start_position > 0 {
+            return Ok(self
+                .wal_file
+                .seek(SeekFrom::Start(block_start_position as u64))?);
+        }
+
+        Ok(0)
+    }
+
+    /**
+    Read the physical record from the file system and parse it into a [`BlockRecord`](BlockRecord).
+
+    Returns the parsed [`BlockRecord`](BlockRecord).
+    */
+    fn read_physical_record(&mut self) -> WALIOResult<BlockRecord> {
+        let serializer = bincode::DefaultOptions::new().with_fixint_encoding();
+
+        // Read the header
+        let mut header_buffer = [0; 3];
+        let header_bytes_read = self.wal_file.read(&mut header_buffer)?;
+        if header_bytes_read < HEADER_LENGTH_BYTES {
+            // The end of the file was reached before we were able to read a full header. This
+            // can occur if the log writer died in the middle of writing the record.
+            return Err(WALIOError::IO(io::Error::new(
+                ErrorKind::UnexpectedEof,
+                "Unexpectedly reached the end of the file while attempting to read a header."
+                    .to_string(),
+            )));
+        }
+
+        let data_length: u16 = serializer.deserialize(&header_buffer[0..2])?;
+
+        // Read the payload
+        let mut data_buffer = Vec::with_capacity(data_length as usize);
+        let data_bytes_read = self.wal_file.read(&mut data_buffer)?;
+
+        if data_bytes_read < (data_length as usize) {
+            // The end of the file was reached before we were able to read a full data chunk. This
+            // can occur if the log writer died in the middle of writing the record.
+            return Err(WALIOError::IO(io::Error::new(
+                ErrorKind::UnexpectedEof,
+                "Unexpectedly reached the end of the file while attempting to read the data chunk."
+                    .to_string(),
+            )));
+        }
+
+        // Parse the payload
+        let serialized_block = [header_buffer.to_vec(), data_buffer].concat();
+        let block_record: BlockRecord = BlockRecord::try_from(&serialized_block)?;
+        self.current_cursor_position += header_buffer.len() + data_bytes_read;
+
+        Ok(block_record)
+    }
+
+    /// Log bytes dropped with the provided reason.
+    fn log_drop(num_bytes_dropped: u64, reason: String) {
+        log::error!(
+            "Skipped reading {} bytes. Reason: {}",
+            num_bytes_dropped,
+            &reason
+        );
+    }
+
+    /// Log bytes dropped because corruption was detected.
+    fn log_corruption(num_bytes_corrupted: u64) {
+        WALReader::log_drop(num_bytes_corrupted, "Detected corruption.".to_owned())
     }
 }
