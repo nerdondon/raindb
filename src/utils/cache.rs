27946ce2b22b6e071cfdebbd9d9036089ab5d840
nerdondon <willdon@nerdondon.com>
2021-11-25 21:58:22 +0000

    Add `remove` and `new_id` methods

--- src/utils/cache.rs
+++ src/utils/cache.rs
@@ -1,19 +1,18 @@
 /*!
 This module provides a `Cache` trait for clients to provide their own cache implementations. A cache
 implementing a least-recently-used (LRU) eviction policy is provided.
-
-The goal is to have the cache be thread-safe but, just to make the initial implementation easier, we
-will return [`Arc`](std::sync::Arc) wrapped values when getting cache entries. Eventually, the hope
-is to have the trait just return `V` instead of wrapping the value in `Arc`.
 */
 
 use parking_lot::RwLock;
+use std::fmt::Debug;
 use std::sync::Arc;
 use std::{collections::HashMap, hash::Hash};
 
 use super::linked_list::{LinkedList, SharedNode};
 
+/// Represents an entry in the cache.
 pub trait CacheEntry<V> {
+    /// Get the value of the entry.
     fn get_value(&self) -> &V;
 }
 
@@ -35,12 +34,8 @@ pub trait Cache<K, V> {
     /// Get the cached value for the given key
     fn get(&self, key: &K) -> Option<Box<dyn CacheEntry<V>>>;
 
-    /**
-    Remove the cached value for the given key.
-
-    Returns the value that was stored or `None` if the key was not found in the cache.
-    */
-    fn remove(&mut self, key: &K) -> Option<Box<dyn CacheEntry<V>>>;
+    /// Remove the cached value for the given key.
+    fn remove(&mut self, key: &K);
 
     /**
     A numeric ID for different clients of the cache.
@@ -54,7 +49,12 @@ pub trait Cache<K, V> {
 
 /// The inner struct holding the data structures of the LRU cache that need to be protected by a
 /// lock.
-struct LRUCacheInner<K, V> {
+#[derive(Debug)]
+struct LRUCacheInner<K, V>
+where
+    K: Debug,
+    V: Debug,
+{
     /// The entries of the cache.
     cache_entries: HashMap<K, SharedNode<(K, V)>>,
 
@@ -65,12 +65,23 @@ struct LRUCacheInner<K, V> {
     used item is at the tail of the list.
     */
     lru_list: LinkedList<(K, V)>,
+
+    /// The last used ID.
+    last_id_given: u64,
 }
 
-/// A fixed-size cache that has a least-recently-used eviction policy.
+/**
+A fixed-size cache that has a least-recently-used eviction policy.
+
+# Concurrency
+
+This cache is thread-safe. It utilizes internal locks to synchronize threads.
+*/
+#[derive(Debug)]
 pub struct LRUCache<K, V>
 where
-    K: Hash + Eq,
+    K: Hash + Eq + Debug,
+    V: Debug,
 {
     /// The maximum number of entries this cache can hold.
     capacity: usize,
@@ -86,7 +97,8 @@ where
 /// Public methods
 impl<K, V> LRUCache<K, V>
 where
-    K: Hash + Eq,
+    K: Hash + Eq + Debug,
+    V: Debug,
 {
     /**
     Create a new instance of the [`LRUCache`].
@@ -96,13 +108,14 @@ where
     The provided capacity must be greater than zero or the program will panic.
     */
     pub fn new(capacity: usize) -> Self {
-        if (capacity < 1) {
+        if capacity < 1 {
             panic!("Capacity must be greater than 0");
         }
 
         let inner = LRUCacheInner {
             cache_entries: HashMap::with_capacity(capacity),
             lru_list: LinkedList::new(),
+            last_id_given: 0,
         };
 
         Self {
@@ -166,13 +179,19 @@ where
         }
     }
 
-    fn remove(&mut self, key: &K) -> Option<Box<dyn CacheEntry<V>>> {
-        todo!()
+    fn remove(&mut self, key: &K) {
+        let writable_inner = self.inner.write();
+        let maybe_removed_node = writable_inner.cache_entries.remove(key);
+        match maybe_removed_node {
+            None => return,
+            Some(removed_node) => writable_inner.lru_list.remove_node(removed_node),
+        }
     }
 
-    /// This is an unsharded cache where all clients share a key space.
     fn new_id(&self) -> u64 {
-        0
+        let inner = self.inner.write();
+        inner.last_id_given += 1;
+        return inner.last_id_given;
     }
 }
 
