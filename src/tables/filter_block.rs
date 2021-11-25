3c56998cfea5fe4cedf4b79e452a35bb14b595a3
nerdondon <willdon@nerdondon.com>
2021-11-25 08:49:35 +0000

    Finish code for processing filter byte buffer and add `key_may_match`

--- src/tables/filter_block.rs
+++ src/tables/filter_block.rs
@@ -72,18 +72,30 @@ impl FilterBlockReader {
         let offsets_start_index = u32::decode_fixed(raw_offsets_start) as usize;
         let offsets = FilterBlockReader::deserialize_offsets(&filter_data[offsets_start_index..])?;
 
+        // Split up the filters according to the deserialized offets for more straight-forward
+        // checking later
         let raw_filters = &filter_data[..&filter_data.len() - 4].to_vec();
-        let filters =
-            FilterBlockReader::deserialize_filters(encoded_range_size_exponent, raw_filters)?;
+        let filters = FilterBlockReader::split_filters_with_offset(
+            encoded_range_size_exponent as u32,
+            &offsets,
+            raw_filters,
+        );
 
         Ok(Self {
             filter_policy: Arc::clone(&filter_policy),
+            filters,
             encoded_range_size_exponent,
             offsets,
         })
     }
 
-    pub fn key_may_match(&self, block_offset: u64, key: &[u8]) {
+    /// Returns true if the `key` is in the filter.
+    pub fn key_may_match(&self, block_offset: u64, key: &[u8]) -> bool {
+        if self.filters.len() < 1 {
+            // There are no filters so just return false
+            return false;
+        }
+
         /*
         LevelDB uses `block_offset >> self.encoded_range_size_exponent` as shorthand for the below.
         We expand it here in an effort to increase readability. Intuitively, a right shift is
@@ -91,12 +103,27 @@ impl FilterBlockReader {
         `block_offset / (2 ^ range_size_exponent)`
         */
         let range_size: u64 = 1 << self.encoded_range_size_exponent;
-        let filter_index = block_offset / range_size;
+        let filter_index = (block_offset / range_size) as usize;
+        if filter_index < 0 || filter_index >= self.filters.len() {
+            // Like LevelDB we ignore a filter matching error and force a disk seek when one is
+            // encountered
+            log::warn!("The provided block offset ({}) could not be used to index the filter array. Ignoring the error and returning true so that a disk seek is done.", block_offset);
+        } else {
+            match (*self.filter_policy).key_may_match(key, self.filters[filter_index]) {
+                Err(error) => {
+                    log::warn!("There was an error checking the filter for a match. Ignoring the error and forcing a disk seek. Original error: {}", error);
+                }
+                Ok(filter_result) => return filter_result,
+            }
+        }
+
+        true
     }
 }
 
 /// Private methods
 impl FilterBlockReader {
+    /// Deserialize the offsets array.
     fn deserialize_offsets(raw_offsets: &[u8]) -> TableResult<Vec<u32>> {
         if raw_offsets.len() % 4 != 0 {
             return Err(ReadError::FailedToParse(
@@ -112,10 +139,33 @@ impl FilterBlockReader {
         Ok(offsets)
     }
 
-    fn deserialize_filters(
+    /**
+    Get filters by the provided offsets.
+
+    Filters are stored end to end in the byte buffer. This method uses the provided offsets to
+    find the ranges representing individual filters and splits the filters up accordingly.
+    */
+    fn split_filters_with_offset(
         range_size_exponent: u32,
         offsets: &[u32],
         raw_filters: &[u8],
     ) -> Vec<Vec<u8>> {
+        let range_size: u64 = 1 << range_size_exponent;
+        let filters: Vec<Vec<u8>> = vec![];
+
+        for offset_index in 0..(offsets.len() - 1) {
+            let next_offset_index = offset_index + 1;
+            let filter_start_index = offsets[offset_index] as usize;
+
+            if next_offset_index >= offsets.len() {
+                // This is the only or the last filter so just push the rest of the range
+                filters.push(raw_filters[filter_start_index..].to_vec());
+            } else {
+                let filter_end_index = (offsets[next_offset_index] - 1) as usize;
+                filters.push(raw_filters[filter_start_index..filter_end_index].to_vec())
+            }
+        }
+
+        filters
     }
 }
