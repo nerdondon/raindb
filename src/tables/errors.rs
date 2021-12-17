99a754520174bb4809301ee7ed1e7c50412d6ee8
nerdondon <willdon@nerdondon.com>
2021-12-17 08:20:37 +0000

    Add error enum for errors that occur during table building

--- src/tables/errors.rs
+++ src/tables/errors.rs
@@ -5,6 +5,8 @@ implementations for common errors to enable error propagation.
 
 use std::{fmt, io};
 
+use crate::key::LookupKey;
+
 use super::footer::SIZE_OF_FOOTER_BYTES;
 
 /// Result that wraps [`ReadError`].
@@ -49,5 +51,44 @@ impl fmt::Display for ReadError {
 impl From<io::Error> for ReadError {
     fn from(err: io::Error) -> Self {
         ReadError::IO(err)
+    }
+}
+
+/// Errors that can result from a table file builder operations.
+#[derive(Debug)]
+pub enum BuilderError {
+    /// Variant for IO errors.
+    IO(io::Error),
+
+    /// Variant for operations that were performed when the file was already closed.
+    AlreadyClosed,
+
+    /// Variant for attempting to add a key that is out of order.
+    OutOfOrder(LookupKey, LookupKey),
+}
+
+impl std::error::Error for BuilderError {}
+
+impl fmt::Display for BuilderError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BuilderError::IO(base_err) => write!(f, "{}", base_err),
+            BuilderError::AlreadyClosed => write!(
+                f,
+                "Attempted to perform an operation when the file had already been closed."
+            ),
+            BuilderError::OutOfOrder(added_key, prev_key) => write!(
+                f,
+                "Attempted to add a key but it was out of order. The key is {:?} but the previous key was {:?}.",
+                added_key,
+                prev_key
+            ),
+        }
+    }
+}
+
+impl From<io::Error> for BuilderError {
+    fn from(err: io::Error) -> Self {
+        BuilderError::IO(err)
     }
 }
