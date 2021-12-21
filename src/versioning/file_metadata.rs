8bbf75ea705f0d2b1e1e6d97293b9d87d750c9d6
nerdondon <willdon@nerdondon.com>
2021-12-21 09:01:16 +0000

    Make properties private and add getters and setters that are more convenient

--- src/versioning/file_metadata.rs
+++ src/versioning/file_metadata.rs
@@ -11,22 +11,22 @@ pub(crate) struct FileMetadata {
     allowed_seeks: Option<u64>,
 
     /// The globally increasing, sequential number for on-disk data files.
-    pub(crate) file_number: u64,
+    file_number: u64,
 
     /// The size of the SSTable file in bytes.
     file_size: u64,
 
     /// The smallest internal key served by the table.
-    pub(crate) smallest_key: Option<LookupKey>,
+    smallest_key: Option<LookupKey>,
 
     /// The largest internal key served by the table.
-    pub(crate) largest_key: Option<LookupKey>,
+    largest_key: Option<LookupKey>,
 }
 
-/// Public methods
+/// Crate-only methods
 impl FileMetadata {
     /// Create an empty instance of [`FileMetadata`].
-    pub fn new(file_number: u64) -> Self {
+    pub(crate) fn new(file_number: u64) -> Self {
         Self {
             file_number,
             allowed_seeks: None,
@@ -36,8 +36,8 @@ impl FileMetadata {
         }
     }
 
-    // Set file size.
-    pub fn set_file_size(&mut self, file_size: u64) {
+    /// Set file size.
+    pub(crate) fn set_file_size(&mut self, file_size: u64) {
         // Allowed seeks is one seek per a configured number of bytes read in a seek
         let mut allowed_seeks = file_size / SEEK_DATA_SIZE_THRESHOLD_KIB;
         if allowed_seeks < 100 {
@@ -46,5 +46,47 @@ impl FileMetadata {
 
         self.file_size = file_size;
         self.allowed_seeks = Some(allowed_seeks);
+    }
+
+    /// Get the file size.
+    pub(crate) fn get_file_size(&self) -> u64 {
+        self.file_size
+    }
+
+    /**
+    Get a reference to the smallest key in the file.
+
+    # Panics
+
+    Panics if the `smallest_key` field is `None`.
+    */
+    pub(crate) fn smallest_key(&self) -> &LookupKey {
+        self.smallest_key.as_ref().unwrap()
+    }
+
+    /**
+    Get a reference to the largest key in the file.
+
+    # Panics
+
+    Panics if the `largest_key` field is `None`.
+    */
+    pub(crate) fn largest_key(&self) -> &LookupKey {
+        self.largest_key.as_ref().unwrap()
+    }
+
+    /// Set the file metadata's smallest key.
+    pub(crate) fn set_smallest_key(&mut self, smallest_key: Option<LookupKey>) {
+        self.smallest_key = smallest_key;
+    }
+
+    /// Set the file metadata's largest key.
+    pub(crate) fn set_largest_key(&mut self, largest_key: Option<LookupKey>) {
+        self.largest_key = largest_key;
+    }
+
+    /// Get the file number.
+    pub(crate) fn file_number(&self) -> u64 {
+        self.file_number
     }
 }
