867346eccb22f773e472ebf06ca295423f865381
nerdondon <willdon@nerdondon.com>
2022-01-16 08:08:21 +0000

    Add module docs

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,3 +1,13 @@
+/*!
+This module contains abstractions for file systems.
+
+Specifically, this module provides a trait that file systems must implement in order to be used
+within RainDB.
+
+Concrete file system implementations include an in-memory file system and a wrapper around the
+Rust [`std::fs`] library which already abstracts POSIX and Windows file system operations.
+*/
+
 mod traits;
 pub use self::traits::{FileSystem, RandomAccessFile, ReadonlyRandomAccessFile};
 
