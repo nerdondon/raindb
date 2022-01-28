9b64268c6efde085324861e8702154fb604bd194
nerdondon <willdon@nerdondon.com>
2022-01-28 08:26:13 +0000

    Finish the `FilesEntryIterator`

--- src/versioning/file_iterators.rs
+++ src/versioning/file_iterators.rs
@@ -5,11 +5,12 @@ file metadata (i.e. [`FileMetadata`]).
 
 use std::sync::Arc;
 
-use crate::errors::RainDBError;
+use crate::errors::{RainDBError, RainDBResult};
 use crate::key::InternalKey;
 use crate::table_cache::TableCache;
+use crate::tables::table::TwoLevelIterator;
 use crate::tables::Table;
-use crate::RainDbIterator;
+use crate::{RainDbIterator, ReadOptions};
 
 use super::file_metadata::FileMetadata;
 
@@ -33,23 +34,107 @@ pub(crate) struct FilesEntryIterator {
     /// The current index into the file list that the cursor is at.
     current_file_index: usize,
 
-    /// The current file being iterated for entries.
-    current_file: Option<Arc<Table>>,
+    /// The current table being iterated for entries.
+    current_table_iter: Option<TwoLevelIterator>,
 
     /// The table cache to retrieve table files from.
     table_cache: Arc<TableCache>,
+
+    /// Options to configure behavior when reading from table files.
+    read_options: ReadOptions,
 }
 
 /// Crate-only methods
 impl FilesEntryIterator {
     /// Create a new instance of [`FilesEntryIterator`].
-    pub(crate) fn new(file_list: Vec<Arc<FileMetadata>>, table_cache: Arc<TableCache>) -> Self {
+    pub(crate) fn new(
+        file_list: Vec<Arc<FileMetadata>>,
+        table_cache: Arc<TableCache>,
+        read_options: ReadOptions,
+    ) -> Self {
         Self {
             file_list,
             current_file_index: 0,
-            current_file: None,
+            current_table_iter: None,
             table_cache,
+            read_options,
         }
+    }
+}
+
+/// Private methods
+impl FilesEntryIterator {
+    /// Check if the index into the file list is pointing at valid location.
+    fn is_file_index_valid(&self) -> bool {
+        self.current_file_index == self.file_list.len()
+    }
+
+    /// Set the table iterator to be used for iteration.
+    fn set_table_iter(&mut self, maybe_new_index: Option<usize>) -> RainDBResult<()> {
+        if maybe_new_index.is_none() || maybe_new_index.unwrap() == self.file_list.len() {
+            self.current_file_index = self.file_list.len();
+            self.current_table_iter = None;
+            return Ok(());
+        }
+
+        if let Some(new_index) = maybe_new_index {
+            if new_index == self.current_file_index {
+                // The file we need to iterate is the same as the current one. No update necessary
+                return Ok(());
+            }
+
+            let table = self
+                .table_cache
+                .find_table(self.file_list[self.current_file_index].file_number())?;
+            self.current_table_iter = Some(Table::iter_with(table, self.read_options.clone()));
+        }
+        Ok(())
+    }
+
+    /// Move forward through any empty files.
+    fn skip_empty_table_files_forward(&mut self) -> RainDBResult<()> {
+        while self.current_table_iter.is_none()
+            || !self.current_table_iter.as_mut().unwrap().is_valid()
+        {
+            if !self.is_file_index_valid() {
+                // We've reached the end of the file list so there are no more tables to iterate
+                self.current_table_iter = None;
+                return Ok(());
+            }
+
+            // Move index iterator to check for the next data block handle
+            self.current_file_index += 1;
+            self.set_table_iter(Some(self.current_file_index))?;
+
+            if self.current_table_iter.is_some() {
+                self.current_table_iter.as_mut().unwrap().seek_to_first()?;
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Move the file index and table iterator backward until we find a non-empty file.
+    fn skip_empty_table_files_backward(&mut self) -> RainDBResult<()> {
+        while self.current_table_iter.is_none()
+            || !self.current_table_iter.as_mut().unwrap().is_valid()
+        {
+            if !self.is_file_index_valid() {
+                // We've reached the end of the file list so there are no more tables to iterate
+                self.current_table_iter = None;
+                return Ok(());
+            }
+
+            // Move index iterator to check for the next data block handle
+            self.current_file_index -= 1;
+            self.set_table_iter(Some(self.current_file_index))?;
+
+            if self.current_table_iter.is_some() {
+                self.current_table_iter.as_mut().unwrap().seek_to_last()?;
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -59,24 +144,32 @@ impl RainDbIterator for FilesEntryIterator {
     type Error = RainDBError;
 
     fn is_valid(&self) -> bool {
-        self.current_file_index < self.file_list.len()
+        self.current_table_iter.is_some() && self.current_table_iter.as_ref().unwrap().is_valid()
     }
 
     fn seek(&mut self, target: &Self::Key) -> Result<(), Self::Error> {
-        let result = super::utils::find_file_with_upper_bound_range(&self.file_list, target);
-        self.current_file_index = match result {
-            Some(index) => index,
-            None => {
-                // Make the iterator invalid if the target was not found
-                self.file_list.len()
-            }
-        };
+        let maybe_new_index =
+            super::utils::find_file_with_upper_bound_range(&self.file_list, target);
+        self.set_table_iter(maybe_new_index)?;
+
+        if self.current_table_iter.is_some() {
+            self.current_table_iter.as_mut().unwrap().seek(target)?;
+        }
+
+        self.skip_empty_table_files_backward()?;
 
         Ok(())
     }
 
     fn seek_to_first(&mut self) -> Result<(), Self::Error> {
         self.current_file_index = 0;
+        self.set_table_iter(Some(self.current_file_index))?;
+
+        if self.current_table_iter.is_some() {
+            self.current_table_iter.as_mut().unwrap().seek_to_first()?;
+        }
+
+        self.skip_empty_table_files_forward()?;
 
         Ok(())
     }
@@ -87,40 +180,51 @@ impl RainDbIterator for FilesEntryIterator {
         } else {
             self.file_list.len() - 1
         };
+        self.set_table_iter(Some(self.current_file_index))?;
+
+        if self.current_table_iter.is_some() {
+            self.current_table_iter.as_mut().unwrap().seek_to_last()?;
+        }
+
+        self.skip_empty_table_files_backward()?;
 
         Ok(())
     }
 
     fn next(&mut self) -> Option<(&Self::Key, &Vec<u8>)> {
-        // The iterator is not valid, don't do anything
         if !self.is_valid() {
             return None;
         }
 
-        self.current_file_index += 1;
-
-        // Return the next item if the iterator is still valid
-        if self.is_valid() {
-            return self.current();
+        if self.current_table_iter.as_mut().unwrap().next().is_none() {
+            if let Err(error) = self.skip_empty_table_files_forward() {
+                log::error!(
+                    "There was an error skipping forward. Original error: {}",
+                    error
+                );
+                return None;
+            }
         }
 
-        None
+        self.current_table_iter.as_mut().unwrap().current()
     }
 
     fn prev(&mut self) -> Option<(&Self::Key, &Vec<u8>)> {
-        // The iterator is not valid, don't do anything
         if !self.is_valid() {
             return None;
         }
 
-        self.current_file_index -= 1;
-
-        // Return the previous item if the iterator is still valid
-        if self.is_valid() {
-            return self.current();
+        if self.current_table_iter.as_mut().unwrap().prev().is_none() {
+            if let Err(error) = self.skip_empty_table_files_backward() {
+                log::error!(
+                    "There was an error skipping backward. Original error: {}",
+                    error
+                );
+                return None;
+            }
         }
 
-        None
+        self.current_table_iter.as_mut().unwrap().current()
     }
 
     fn current(&self) -> Option<(&Self::Key, &Vec<u8>)> {
@@ -128,7 +232,6 @@ impl RainDbIterator for FilesEntryIterator {
             return None;
         }
 
-        let current_file = Arc::clone(&self.file_list[self.current_file_index]);
-        todo!()
+        self.current_table_iter.as_ref().unwrap().current()
     }
 }
