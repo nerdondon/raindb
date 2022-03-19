15c7ee725113e78a39ef1f7ff03935a5d6ead504
nerdondon <willdon@nerdondon.com>
2022-03-19 07:30:44 +0000

    Add tests and fix bug

--- src/utils/bytes.rs
+++ src/utils/bytes.rs
@@ -41,7 +41,7 @@ impl BinarySeparable for &[u8] {
         // Check if there is a byte that we can increment to generate a separator from the prefix
         let diff_byte = smaller[diff_idx];
         if diff_byte < u8::MAX && diff_byte + 1 < greater[diff_idx] {
-            let mut separator = smaller[..diff_idx].to_vec();
+            let mut separator = smaller[..=diff_idx].to_vec();
             separator[diff_idx] += 1;
             assert!(separator.as_slice() < greater);
 
@@ -65,5 +65,53 @@ impl BinarySeparable for &[u8] {
 
         // There was a run of u8::MAX
         successor
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn for_a_byte_buffer_when_one_buffer_is_a_prefix_can_find_the_shortest_separator() {
+        let smaller = b"batman";
+        let greater = b"batmann";
+
+        let actual =
+            BinarySeparable::find_shortest_separator(smaller.as_slice(), greater.as_slice());
+
+        assert_eq!(smaller, actual.as_slice());
+    }
+
+    #[test]
+    fn for_a_byte_buffer_when_the_buffers_are_correctly_ordered_can_find_the_shortest_separator() {
+        let smaller = b"ryu";
+        let greater = b"tumtum";
+
+        let actual =
+            BinarySeparable::find_shortest_separator(smaller.as_slice(), greater.as_slice());
+
+        assert_eq!(b"s", actual.as_slice());
+    }
+
+    #[test]
+    fn for_a_byte_buffer_when_args_are_misordered_returns_the_first_arg() {
+        let smaller = b"abc";
+        let greater = b"def";
+
+        let actual =
+            BinarySeparable::find_shortest_separator(greater.as_slice(), smaller.as_slice());
+
+        assert_eq!(greater, actual.as_slice());
+    }
+
+    #[test]
+    fn for_a_byte_buffer_can_find_a_shortest_successor() {
+        let buf = b"abc";
+
+        let actual = BinarySeparable::find_shortest_successor(buf.as_slice());
+
+        assert_eq!(b"b", actual.as_slice());
     }
 }
