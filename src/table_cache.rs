864a96fc2e74422abf0881b6e90ff12fe2ca8ff0
nerdondon <willdon@nerdondon.com>
2022-05-06 07:09:32 +0000

    Add tests for the table cache

--- src/table_cache.rs
+++ src/table_cache.rs
@@ -91,3 +91,86 @@ impl fmt::Debug for TableCache {
             .finish()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use integer_encoding::FixedInt;
+    use pretty_assertions::assert_eq;
+
+    use crate::tables::TableBuilder;
+    use crate::{Operation, RainDbIterator};
+
+    use super::*;
+
+    const NUM_ENTRIES_PER_TABLE: usize = 3000;
+
+    #[test]
+    fn can_find_a_table_via_the_cache() {
+        const MAX_BLOCK_SIZE_BYTES: usize = 256;
+        let mut options = DbOptions::with_memory_env();
+        options.max_block_size = MAX_BLOCK_SIZE_BYTES;
+
+        create_tables(&options, 13, NUM_ENTRIES_PER_TABLE);
+        let cache = TableCache::new(options, 10);
+
+        for idx in 0..13 {
+            let table = cache.find_table(idx).unwrap();
+            let mut iter = Table::iter_with(table, ReadOptions::default());
+            assert!(iter.seek_to_first().is_ok());
+            assert!(iter.seek_to_last().is_ok());
+        }
+
+        // Cache should be populated now and we should be able to get a table from the cache
+        let table = cache.find_table(5).unwrap();
+        let mut iter = Table::iter_with(table, ReadOptions::default());
+        assert!(iter.seek_to_first().is_ok());
+        assert!(iter.seek_to_last().is_ok());
+
+        let table = cache.find_table(0).unwrap();
+        let mut iter = Table::iter_with(table, ReadOptions::default());
+        assert!(iter.seek_to_first().is_ok());
+        assert!(iter.seek_to_last().is_ok());
+    }
+
+    #[test]
+    fn can_get_a_value_via_the_cache() {
+        const MAX_BLOCK_SIZE_BYTES: usize = 256;
+        let mut options = DbOptions::with_memory_env();
+        options.max_block_size = MAX_BLOCK_SIZE_BYTES;
+        create_tables(&options, 13, NUM_ENTRIES_PER_TABLE);
+        let cache = TableCache::new(options, 10);
+        let expected = u64::encode_fixed_vec(111_999);
+
+        let actual = cache
+            .get(
+                &ReadOptions::default(),
+                3,
+                &InternalKey::new_for_seeking(111_999.to_string().as_bytes().to_vec(), 3),
+            )
+            .unwrap();
+
+        assert_eq!(Some(expected), actual);
+    }
+
+    /// Create the specified number of tables.
+    fn create_tables(options: &DbOptions, num_tables: u64, num_entries_per_table: usize) {
+        let mut current_entry: u64 = 100_000;
+        for idx in 0..num_tables {
+            let mut builder = TableBuilder::new(options.clone(), idx).unwrap();
+            for _ in 0..num_entries_per_table {
+                let key = InternalKey::new(
+                    current_entry.to_string().as_bytes().to_vec(),
+                    idx as u64,
+                    Operation::Put,
+                );
+                builder
+                    .add_entry(Rc::new(key), &u64::encode_fixed_vec(current_entry as u64))
+                    .unwrap();
+                current_entry += 1;
+            }
+            builder.finalize().unwrap();
+        }
+    }
+}
