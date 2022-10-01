f49c227560e4239f7d294c0e711e62799cd866b6
nerdondon <willdon@nerdondon.com>
2022-10-01 07:45:56 +0000

    Add test for switching iteration direction from backwards to forwards

--- src/versioning/file_iterators.rs
+++ src/versioning/file_iterators.rs
@@ -1095,6 +1095,54 @@ mod merging_iterator_tests {
         );
     }
 
+    #[test]
+    fn can_iterate_forwards_after_iterating_backward() {
+        let options = DbOptions::with_memory_env();
+        let table_cache = Arc::new(TableCache::new(options.clone(), 1000));
+        let version = create_test_version(options, &table_cache);
+        let version_iterators = version
+            .get_representative_iterators(&ReadOptions::default())
+            .unwrap();
+        let mut iter = MergingIterator::new(version_iterators);
+
+        assert!(iter
+            .seek(&InternalKey::new(
+                "y".as_bytes().to_vec(),
+                58,
+                Operation::Put
+            ))
+            .is_ok());
+        let (actual_key, _) = iter.current().unwrap();
+        assert_eq!(
+            actual_key,
+            &InternalKey::new("y".as_bytes().to_vec(), 58, Operation::Put)
+        );
+
+        assert!(iter.prev().is_some());
+        let (actual_key, _) = iter.current().unwrap();
+        assert_eq!(
+            actual_key,
+            &InternalKey::new("y".as_bytes().to_vec(), 79, Operation::Put),
+            "Expected to seek to prev after a random seek"
+        );
+
+        assert!(iter.next().is_some());
+        let (actual_key, _) = iter.current().unwrap();
+        assert_eq!(
+            actual_key,
+            &InternalKey::new("y".as_bytes().to_vec(), 58, Operation::Put),
+            "Expected to seek to previous from an iterator that was iterating forward"
+        );
+
+        assert!(iter.next().is_some());
+        let (actual_key, _) = iter.current().unwrap();
+        assert_eq!(
+            actual_key,
+            &InternalKey::new("z1".as_bytes().to_vec(), 45, Operation::Put),
+            "Expected other child iterators to be correctly set after changing iteration direction"
+        );
+    }
+
     /// Creates version used to hold files for testing.
     fn create_test_version(db_options: DbOptions, table_cache: &Arc<TableCache>) -> Version {
         let mut version = Version::new(db_options.clone(), table_cache, 200, 30);
